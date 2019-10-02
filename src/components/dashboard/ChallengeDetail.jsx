112065cae89706a6ac5b1fd4d85d5e1304bdaa6b
Jennifer V Phan <jennyphan181295@gmail.com>
2019-10-02 22:53:51 +0200

    questionaire part after sign up

---
+++ src/components/dashboard/ChallengeDetail.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ChallengeDetail.css';
+
+export default class ChallengeDetail extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
