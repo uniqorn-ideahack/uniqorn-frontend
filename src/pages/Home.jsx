f31714db8bbaca3274f9a08385af10247127f990
Lukas <s363397@stud-mail.uni-wuerzburg.de>
2019-10-03 10:06:57 +0200

    Add logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import "./Home.css";
 import MainLayout from "../components/layout/MainLayout";
+import Logo from "../Logo";
 
 export default class Home extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             user: JSON.parse(localStorage.getItem('user')),
         }
     }
@@ -13,23 +14,26 @@ export default class Home extends Component {
     render() {
         return (
             <MainLayout>
-                {(this.state.user)?
+                {(this.state.user) ?
                     <main>
                         <h1>Hello Tim!</h1>
                         <h3>Your journey to lose weight: Day 7 of 30</h3>
                         <progress max="100" value="40"></progress>
                         <h2>Next step</h2>
                     </main>
-                :
+                    :
                     <main>
+                        <Logo width={250} height={250}/>
                         <h1>Better you.</h1>
-                        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, 
-                            vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. 
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.   
+                        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat,
+                            vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim
+                            qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                            tincidunt ut laoreet dolore magna aliquam erat volutpat.
                         </p>
                     </main>
                 }
-                
+
             </MainLayout>
         )
     }
